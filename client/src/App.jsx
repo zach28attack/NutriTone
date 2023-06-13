c218992b9642cdd47b88ff1e661dea5bbc24d676
Zachary Casares <rrumble250@gmail.com>
2023-06-13 13:36:12 -0400

    add homepage route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import {Outlet} from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>App layout</h1>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
