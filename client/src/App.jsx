cdc9685156e668105c3f22c3e93cb07c78fdaa23
Zachary Casares <rrumble250@gmail.com>
2023-06-14 11:31:07 -0400

    offset pages by 5rem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import {Outlet} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.jsx";
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Outlet />
-    </div>
+      <div className="page">
+        <Outlet />
+      </div>
+    </>
   );
 }
 
