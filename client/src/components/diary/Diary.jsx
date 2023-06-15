1688b59400504d9f5fc24b646af242b78ddfe18e
Zachary Casares <rrumble250@gmail.com>
2023-06-15 14:12:54 -0400

    add diary to homepage

---
+++ client/src/components/diary/Diary.jsx
@@ -0,0 +1,29 @@
+import Class from "./Diary.module.css";
+import DiaryItem from "./DiaryItem";
+import AddIcon from "./AddIcon";
+function Diary(props) {
+  let totalCalories = 0;
+  for (let i = 0; i < props.diaries.length; i++) {
+    totalCalories += props.diaries[i].calories;
+  }
+  return (
+    <div className={Class.container}>
+      <header className={Class.header}>
+        <div>
+          {props.timeOfDay}
+          {": "}
+          {totalCalories}
+        </div>
+        <AddIcon />
+      </header>
+      <main className={Class.items}>
+        {props.diaries.map((item) => (
+          <DiaryItem timeOfDay={props.timeOfDay} name={item.name} servings={item.servings} calories={item.calories} />
+        ))}
+      </main>
+      <div className={Class.addBtn}></div>
+    </div>
+  );
+}
+
+export default Diary;
