701fdcc6e9e062740343f9c28888f7e25b4a1e5c
Zachary Casares <rrumble250@gmail.com>
2023-06-15 14:13:22 -0400

    render diaries

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
 import DiarySummary from "../components/diary/DiarySummary";
+import Diary from "../components/diary/Diary";
 
 function HomePage() {
+  const diaries = [{name: "Egg", servings: 1, calories: 205}];
+  const diaries2 = [
+    {name: "Steak and eggs", servings: 99, calories: 550},
+    {name: "Steak and eggs", servings: 99, calories: 550},
+    {name: "Steak and eggs", servings: 99, calories: 550},
+  ];
+  const diaries3 = [{name: "", servings: undefined, calories: undefined}];
   return (
     <>
       <DiarySummary />
+      <Diary timeOfDay={"Breakfast"} diaries={diaries} />
+      <Diary timeOfDay={"Lunch"} diaries={diaries2} />
+      <Diary timeOfDay={"Dinner"} diaries={diaries3} />
     </>
   );
 }
