13077e5ac2610ceca05ad0837d2e7f703603b84e
Zachary Casares <rrumble250@gmail.com>
2023-06-15 14:11:45 -0400

    add custom add icon

---
+++ client/src/components/diary/AddIcon.jsx
@@ -0,0 +1,10 @@
+import Class from "./AddIcon.module.css";
+function AddIcon() {
+  return (
+    <div className={Class.container}>
+      <div className={Class.line1}></div>
+      <div className={Class.line2}></div>
+    </div>
+  );
+}
+export default AddIcon;
