aa52b8c9dab1f6489e02d885cb3c87f1cad022e2
Zachary Casares <rrumble250@gmail.com>
2023-06-17 15:37:43 -0400

    create login component

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,5 +1,22 @@
+import Class from "./Login.module.css";
 function Login() {
-  return;
+  return (
+    <form className={Class.form}>
+      <label className={Class.label}>Username</label>
+      <input className={Class.input}></input>
+
+      <label className={Class.label}>Password</label>
+      <input className={Class.input}></input>
+
+      <button className={Class.login}>Submit</button>
+
+      <div className={Class.formLinks}>
+        <a className={Class.formLink}>Forgot Username</a>
+        <div className={Class.divider}></div>
+        <a className={Class.formLink}>Forgot Password</a>
+      </div>
+    </form>
+  );
 }
 
 export default Login;
