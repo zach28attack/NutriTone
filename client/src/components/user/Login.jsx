46f8737150b3df2a6747a2f5cbbf830e5eb11ef8
Zachary Casares <rrumble250@gmail.com>
2023-06-17 15:45:53 -0400

    style form components

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,21 +1,24 @@
 import Class from "./Login.module.css";
 function Login() {
   return (
-    <form className={Class.form}>
-      <label className={Class.label}>Username</label>
-      <input className={Class.input}></input>
+    <div className={Class.container}>
+      <h1 className={Class.title}>Login</h1>
+      <form className={Class.form}>
+        <label className={Class.label}>Username</label>
+        <input className={Class.input}></input>
 
-      <label className={Class.label}>Password</label>
-      <input className={Class.input}></input>
+        <label className={Class.label}>Password</label>
+        <input className={Class.input}></input>
 
-      <button className={Class.login}>Submit</button>
+        <button className={Class.login}>Submit</button>
 
-      <div className={Class.formLinks}>
-        <a className={Class.formLink}>Forgot Username</a>
-        <div className={Class.divider}></div>
-        <a className={Class.formLink}>Forgot Password</a>
-      </div>
-    </form>
+        <div className={Class.formLinks}>
+          <a className={Class.formLink}>Forgot Username</a>
+          <div className={Class.divider}></div>
+          <a className={Class.formLink}>Forgot Password</a>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -2,21 +2,24 @@ import Class from "./Signup.module.css";
 
 function Signup() {
   return (
-    <form className={Class.form}>
-      <label className={Class.label}>Email</label>
-      <input className={Class.input} />
+    <div className={Class.container}>
+      <h1 className={Class.title}>Sign up</h1>
+      <form className={Class.form}>
+        <label className={Class.label}>Email</label>
+        <input className={Class.input} />
 
-      <label className={Class.label}>Username</label>
-      <input className={Class.input} />
+        <label className={Class.label}>Username</label>
+        <input className={Class.input} />
 
-      <label className={Class.label}>Password</label>
-      <input className={Class.input} />
+        <label className={Class.label}>Password</label>
+        <input className={Class.input} />
 
-      <label className={Class.label}>Confirm Password</label>
-      <input className={Class.input} />
+        <label className={Class.label}>Confirm Password</label>
+        <input className={Class.input} />
 
-      <button className={Class.submit}>Submit</button>
-    </form>
+        <button className={Class.submit}>Submit</button>
+      </form>
+    </div>
   );
 }
 
