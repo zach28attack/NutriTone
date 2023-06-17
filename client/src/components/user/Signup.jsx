b4fb0f02adedd66e61a820f16d014f9df0fc00dd
Zachary Casares <rrumble250@gmail.com>
2023-06-17 15:37:52 -0400

    create signup component

--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -1,5 +1,23 @@
+import Class from "./Signup.module.css";
+
 function Signup() {
-  return;
+  return (
+    <form className={Class.form}>
+      <label className={Class.label}>Email</label>
+      <input className={Class.input} />
+
+      <label className={Class.label}>Username</label>
+      <input className={Class.input} />
+
+      <label className={Class.label}>Password</label>
+      <input className={Class.input} />
+
+      <label className={Class.label}>Confirm Password</label>
+      <input className={Class.input} />
+
+      <button className={Class.submit}>Submit</button>
+    </form>
+  );
 }
 
 export default Signup;
