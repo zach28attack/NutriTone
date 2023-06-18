d22658c616e6c7d1d62d07ae4d57b4a1906006d2
Zachary Casares <rrumble250@gmail.com>
2023-06-18 18:36:15 -0400

    create login route

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const {signup, logout} = require("./controllers/users");
+const {signup, logout, login} = require("./controllers/users");
 const {verifyToken} = require("./authenticate");
 
 app.use(bodyParser.json());
 
 app.post("/user/signup", signup);
+
+app.get("/user/login", login);
 
 app.get("/user/logout", logout);
 
