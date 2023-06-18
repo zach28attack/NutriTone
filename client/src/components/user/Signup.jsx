20c6d09abdc0b26786c25903e43f3c44b58c1103
Zachary Casares <rrumble250@gmail.com>
2023-06-18 19:48:38 -0400

    capture input and call signup api

--- client/src/components/user/Signup.jsx
+++ client/src/components/user/Signup.jsx
@@ -1,21 +1,60 @@
 import Class from "./Signup.module.css";
+import {useState} from "react";
+import {signup} from "../../../api";
+import Cookies from "js-cookie";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const emailInputHandler = (val) => {
+    setEmail(val.target.value);
+  };
+  const usernameInputHandler = (val) => {
+    setUsername(val.target.value);
+  };
+  const passwordInputHandler = (val) => {
+    setPassword(val.target.value);
+  };
+  const passwordConfirmationInputHandler = (val) => {
+    setPasswordConfirmation(val.target.value);
+  };
+
+  const formSubmitHandler = async (e) => {
+    e.preventDefault();
+    console.log("input:", email, username, password, passwordConfirmation);
+    if (email && username && password && passwordConfirmation) {
+      console.log("signup api call");
+      const success = await signup(email, username, password, passwordConfirmation);
+      if (success) {
+        console.log("User signed up", "Cookies:", Cookies.get("userId"));
+        //TODO: after signup nav to homepage
+      }
+    }
+  };
+
   return (
     <div className={Class.container}>
       <h1 className={Class.title}>Sign up</h1>
-      <form className={Class.form}>
+      <form className={Class.form} onSubmit={formSubmitHandler}>
         <label className={Class.label}>Email</label>
-        <input className={Class.input} />
+        <input className={Class.input} type="text" name="email" onChange={emailInputHandler} />
 
         <label className={Class.label}>Username</label>
-        <input className={Class.input} />
+        <input className={Class.input} type="text" name="username" onChange={usernameInputHandler} />
 
         <label className={Class.label}>Password</label>
-        <input className={Class.input} />
+        <input className={Class.input} type="text" name="password" onChange={passwordInputHandler} />
 
         <label className={Class.label}>Confirm Password</label>
-        <input className={Class.input} />
+        <input
+          className={Class.input}
+          type="text"
+          name="passwordConfirmation"
+          onChange={passwordConfirmationInputHandler}
+        />
 
         <button className={Class.submit}>Submit</button>
       </form>
