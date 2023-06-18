33281bef05c5a4863cdbf9a34fc89609524a9706
Zachary Casares <rrumble250@gmail.com>
2023-06-18 18:34:36 -0400

    create user logout

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const {signup} = require("./controllers/users");
+const {signup, logout} = require("./controllers/users");
+const {verifyToken} = require("./authenticate");
 
 app.use(bodyParser.json());
 
-app.get("/user/signup", signup);
+app.post("/user/signup", signup);
+
+app.get("/user/logout", logout);
 
 app.listen(3000);
