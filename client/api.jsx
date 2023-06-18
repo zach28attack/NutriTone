746df8794fb516f2bda69042cddadaddfd8f4142
Zachary Casares <rrumble250@gmail.com>
2023-06-18 19:48:02 -0400

    fix incorrect fetch header

--- client/api.jsx
+++ client/api.jsx
@@ -1,25 +1,29 @@
 import Cookies from "js-cookie";
 
-exports.signup = async (email, username, password, passwordConfirmation) => {
-  const result = await fetch("localhost:3000/user/signup", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
-      email: email,
-      username: username,
-      password: password,
-      passwordConfirmation: passwordConfirmation,
-    }),
-  });
+export async function signup(email, username, password, passwordConfirmation) {
+  try {
+    const result = await fetch("http://localhost:3000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        username: username,
+        password: password,
+        passwordConfirmation: passwordConfirmation,
+      }),
+    });
 
-  if (result.ok) {
-    const data = await result.json();
-    Cookies.set("userId", data.id, {expires: 1});
-    Cookies.set("token", data.token, {expires: 1});
-    return true;
-  } else {
-    return false;
+    if (result.ok) {
+      const data = await result.json();
+      Cookies.set("userId", data.id, {expires: 1});
+      Cookies.set("token", data.token, {expires: 1});
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
   }
-};
+}
