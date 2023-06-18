3a2d181c143e326f7fd55101ebe56bd86979ffbd
Zachary Casares <rrumble250@gmail.com>
2023-06-17 20:07:22 -0400

    create user/signup route

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const User = require("./models/user");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res, next) => {
+app.use(bodyParser.json());
+
+app.get("/user/signup", async (req, res, next) => {
   const user = new User();
-  user.saveNew();
+  user.email = req.body.email;
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  const result = await user.saveNew();
+
+  if (result) {
+    res.status(200).json({
+      id: result.insertedId,
+    });
+  } else {
+    res.status(400).json();
+    console.error("failed to save");
+  }
 });
 
 app.listen(3000);
