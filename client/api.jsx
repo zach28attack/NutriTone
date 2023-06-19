2e740311ca27ee7a993a1699e297cf883e1072c7
Zachary Casares <rrumble250@gmail.com>
2023-06-19 13:45:13 -0400

    add logout api function

--- client/api.jsx
+++ client/api.jsx
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export async function signup(email, username, password, passwordConfirmation) {
   try {
-    const result = await fetch("http://localhost:3000/user", {
+    const response = await fetch("http://localhost:3000/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -15,13 +15,34 @@ export async function signup(email, username, password, passwordConfirmation) {
       }),
     });
 
-    if (result.ok) {
-      const data = await result.json();
+    if (response.ok) {
+      const data = await response.json();
       Cookies.set("userId", data.id, {expires: 1});
       Cookies.set("token", data.token, {expires: 1});
       return true;
     } else {
       return false;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function logout() {
+  try {
+    const response = await fetch("http://localhost:3000/user/logout", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    });
+    if (response.ok) {
+      Cookies.remove("userId");
+      Cookies.remove("token");
+      console.log("cookies removed");
+    } else {
+      console.error("Issue done happened");
     }
   } catch (error) {
     console.error(error);
