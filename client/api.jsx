dafd8275c0288e990ba51e66b96d1ac40d0c9e3a
Zachary Casares <rrumble250@gmail.com>
2023-06-19 14:56:42 -0400

    set login api function

--- client/api.jsx
+++ client/api.jsx
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export async function signup(email, username, password, passwordConfirmation) {
   try {
-    const response = await fetch("http://localhost:3000/user", {
+    const response = await fetch("http://localhost:3000/user/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -43,6 +43,26 @@ export async function logout() {
       console.log("cookies removed");
     } else {
       console.error("Issue done happened");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function login(username, password) {
+  try {
+    const response = await fetch("http://localhost:3000/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username: username, password: password}),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      Cookies.set("token", data.token);
+      Cookies.set("userId", data.id);
+      return true;
     }
   } catch (error) {
     console.error(error);
