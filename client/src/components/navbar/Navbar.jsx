4c25ed253949702a04ec6cf5fb45f4eb93d1f062
Zachary Casares <rrumble250@gmail.com>
2023-06-19 13:45:43 -0400

    add  logout button to nav

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,8 +2,14 @@ import Class from "./Navbar.module.css";
 import {BiLineChartDown} from "react-icons/bi";
 import {FaUserFriends, FaUser} from "react-icons/fa";
 import {GiWhistle} from "react-icons/gi";
+import {logout} from "../../../api";
 
 function Navbar() {
+  const logoutHandler = () => {
+    logout();
+    console.log("logout ran");
+  };
+
   return (
     <div className={Class.navBanner}>
       <div className={Class.border}>
@@ -11,6 +17,7 @@ function Navbar() {
         <FaUserFriends className={Class.icon} />
         <GiWhistle className={Class.icon} />
         <FaUser className={Class.icon} />
+        <button onClick={logoutHandler}>logout</button>
       </div>
     </div>
   );
