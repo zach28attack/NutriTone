84b6e377342734b6e8c76839cf6606e36a4a0c05
Zachary Casares <rrumble250@gmail.com>
2023-06-19 14:57:54 -0400

    add login user logic

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -22,6 +22,28 @@ exports.signup = async (req, res, next) => {
   }
 };
 
+exports.login = async (req, res, next) => {
+  const user = new User();
+  user.email = req.body.email;
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  if (user.username && user.password) {
+    try {
+      await user.validateUser();
+      res.status(200).json({
+        id: user.id,
+        token: user.token,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(400).json();
+    }
+  } else {
+    res.status(400).json();
+  }
+};
+
 exports.logout = async (req, res, next) => {
   const user = req.user;
   const success = await user.revokeToken();
