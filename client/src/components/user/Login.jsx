6ac86723c6061a17e28f9439fedeef4524698dff
Zachary Casares <rrumble250@gmail.com>
2023-06-19 14:56:58 -0400

    capture iinput and call login function

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,16 +1,38 @@
 import Class from "./Login.module.css";
+import {useState} from "react";
+import {login} from "../../../api";
+import {useNavigate} from "react-router-dom";
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameHandler = (val) => {
+    setUsername(val.target.value);
+  };
+  const passwordHandler = (val) => {
+    setPassword(val.target.value);
+  };
+  const navigate = useNavigate();
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    const success = await login(username, password);
+    if (success) {
+      navigate("/");
+    }
+  };
+
   return (
     <div className={Class.container}>
       <h1 className={Class.title}>Login</h1>
-      <form className={Class.form}>
+      <form className={Class.form} onSubmit={loginHandler}>
         <label className={Class.label}>Username</label>
-        <input className={Class.input}></input>
+        <input className={Class.input} onChange={usernameHandler}></input>
 
         <label className={Class.label}>Password</label>
-        <input className={Class.input}></input>
+        <input className={Class.input} onChange={passwordHandler}></input>
 
-        <button className={Class.login}>Submit</button>
+        <input type="submit" className={Class.login} />
 
         <div className={Class.formLinks}>
           <a className={Class.formLink}>Forgot Username</a>
