9b30cd3b62572398dcf8d8bfe7b5e147fa4396b8
Zachary Casares <rrumble250@gmail.com>
2023-06-20 14:39:45 -0400

    add getOneDiary api function

---
+++ client/src/apis/diaryApi.jsx
@@ -0,0 +1,16 @@
+import Cookies from "js-cookie";
+
+export async function getOneDiary() {
+  const res = await fetch("http://localhost:3000/diary", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${Cookies.get("token")}`,
+    },
+    body: JSON.stringify({date: "1/1/2023"}),
+  });
+  if (res) {
+    const data = await res.json();
+    return data.items;
+  }
+}
