833541226ca6e97537f2d3ad58b48dcf30039a65
Zachary Casares <rrumble250@gmail.com>
2023-06-20 18:25:12 -0400

    send diaries in response

--- server/controllers/diaries.js
+++ server/controllers/diaries.js
@@ -21,10 +21,18 @@ exports.getDiary = async (req, res, next) => {
 };
 
 exports.getTenDiaries = async (req, res, next) => {
-  const diary = new Diary();
-  diary.userId = req.user.id;
-  diary.date = req.body.date;
-  const diaries = await diary.getTenDiaries();
+  try {
+    const diary = new Diary();
+    diary.userId = req.user.id;
+    diary.date = req.body.date;
+    const diaries = await diary.getTenDiaries();
+    res.status(200).json({
+      diaries: diaries,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json();
+  }
 };
 
 exports.saveItemToDiary = async (req, res, next) => {
