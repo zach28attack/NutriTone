7200d7c1374740ae059cbd1ac6b2a50c100ea6d2
Zachary Casares <rrumble250@gmail.com>
2023-06-20 14:40:57 -0400

    add getDiary controller action

--- server/controllers/diaries.js
+++ server/controllers/diaries.js
@@ -1,3 +1,30 @@
 const Diary = require("../models/diary");
 
-exports.getDiaries = async (req, res, next) => {};
+exports.getDiary = async (req, res, next) => {
+  try {
+    const diary = new Diary();
+    diary.userId = req.user.id;
+    diary.date = req.body.date;
+    const success = await diary.getOneDiary();
+    if (success) {
+      res.status(200).json({
+        items: diary.items,
+      });
+    } else {
+      res.status(200).json({
+        items: [{calories: 0, name: ""}],
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.saveItemToDiary = async (req, res, next) => {
+  const diary = new Diary();
+  diary.userId = req.user.id;
+
+  const {day, month, year} = req.body;
+  diary.date = `${month}/${day}/${year}`;
+  diary.saveItemToDiary();
+};
