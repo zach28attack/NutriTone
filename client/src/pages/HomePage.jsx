fe558e41314c6c4e47bbaae6bf2900063a2a96c6
Zachary Casares <rrumble250@gmail.com>
2023-06-20 14:40:28 -0400

    call getOneDiary api function and set items

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,20 +1,30 @@
 import DiarySummary from "../components/diary/DiarySummary";
 import Diary from "../components/diary/Diary";
+import {getOneDiary} from "../apis/diaryApi";
+import {useState, useEffect} from "react";
 
 function HomePage() {
-  const diaries = [{name: "Egg", servings: 1, calories: 205}];
-  const diaries2 = [
-    {name: "Steak and eggs", servings: 99, calories: 550},
-    {name: "Steak and eggs", servings: 99, calories: 550},
-    {name: "Steak and eggs", servings: 99, calories: 550},
-  ];
-  const diaries3 = [{name: "", servings: undefined, calories: undefined}];
+  // const diaries = [{name: "Egg", servings: 1, calories: 205}];
+  const [items, setItems] = useState([{name: "", servings: undefined, calories: undefined}]);
+  const [items2, setItems2] = useState([{name: "", servings: undefined, calories: undefined}]);
+  const [items3, setItems3] = useState([{name: "", servings: undefined, calories: undefined}]);
+  const getItemsFromDiary = async () => {
+    const items = await getOneDiary();
+    // will return all items from a given day
+    // filter each item by tOD
+    setItems(items);
+  };
+
+  useEffect(() => {
+    getItemsFromDiary();
+  }, []);
+
   return (
     <>
       <DiarySummary />
-      <Diary timeOfDay={"Breakfast"} diaries={diaries} />
-      <Diary timeOfDay={"Lunch"} diaries={diaries2} />
-      <Diary timeOfDay={"Dinner"} diaries={diaries3} />
+      <Diary timeOfDay={"Breakfast"} diaries={items} />
+      <Diary timeOfDay={"Lunch"} diaries={items2} />
+      <Diary timeOfDay={"Dinner"} diaries={items3} />
     </>
   );
 }
