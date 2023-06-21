15dd74f7c90072a9bb253bcc5f2a37485e27516a
Zachary Casares <rrumble250@gmail.com>
2023-06-21 17:25:13 -0400

    rotate btn when clicked

--- client/src/components/diary/AddIcon.jsx
+++ client/src/components/diary/AddIcon.jsx
@@ -1,10 +1,22 @@
 import Class from "./AddIcon.module.css";
+import {useState} from "react";
+import {createPortal} from "react-dom";
+import DiaryForm from "./DiaryForm";
+
 function AddIcon() {
+  const addItemClickHandler = () => {
+    setIsActive(!isActive);
+  };
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className={Class.container}>
-      <div className={Class.line1}></div>
-      <div className={Class.line2}></div>
-    </div>
+    <>
+      <div className={`${Class.container} ${isActive && Class.active}`} onClick={addItemClickHandler}>
+        <div className={Class.line1}></div>
+        <div className={Class.line2}></div>
+      </div>
+      {isActive && createPortal(<DiaryForm isActive={isActive} />, document.querySelector("#form-location"))}
+    </>
   );
 }
 export default AddIcon;
