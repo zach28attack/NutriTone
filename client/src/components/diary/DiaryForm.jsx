ce2790382310652139161240a6d2c24329afe5a4
Zachary Casares <rrumble250@gmail.com>
2023-06-25 14:07:39 -0400

    send update request on form submit

--- client/src/components/diary/DiaryForm.jsx
+++ client/src/components/diary/DiaryForm.jsx
@@ -1,15 +1,28 @@
 import Class from "./DiaryForm.module.css";
 import {useState} from "react";
 import {BsFillArrowRightCircleFill} from "react-icons/bs";
+import {updateItem} from "../../apis/diaryApi";
 
-function DiaryForm({addItem, setIsActive, timeOfDay}) {
+function DiaryForm({addItem, setIsActive, timeOfDay, isEditing, item, _id, editItem}) {
   const formSubmitHandlerClick = () => {
-    addItem({name: inputName, calories: inputCalories, servings: inputServings, timeOfDay: timeOfDay});
-    setIsActive(false);
+    const item = {name: inputName, calories: inputCalories, servings: inputServings, timeOfDay: timeOfDay, _id: _id};
+    if (isEditing) {
+      updateItem(item);
+      editItem(item.name, item.servings, item.calories);
+    }
+    if (!isEditing) {
+      addItem(item);
+      setIsActive(false);
+    }
   };
   const formSubmitHandlerKeypress = (e) => {
-    if (e.key === "Enter") {
-      addItem({name: inputName, calories: inputCalories, servings: inputServings, timeOfDay: timeOfDay});
+    const item = {name: inputName, calories: inputCalories, servings: inputServings, timeOfDay: timeOfDay, _id: _id};
+    if (isEditing && e.key === "Enter") {
+      updateItem(item);
+      editItem(item.name, item.servings, item.calories);
+    }
+    if (!isEditing && e.key === "Enter") {
+      addItem(item);
       setIsActive(false);
     }
   };
@@ -22,23 +35,24 @@ function DiaryForm({addItem, setIsActive, timeOfDay}) {
   const inputServingsHandler = (e) => {
     setInputServings(e.target.value);
   };
-  const [inputName, setInputName] = useState();
-  const [inputCalories, setInputCalories] = useState();
-  const [inputServings, setInputServings] = useState();
+
+  const [inputName, setInputName] = useState(item ? item.name : "");
+  const [inputCalories, setInputCalories] = useState(item ? item.calories : "");
+  const [inputServings, setInputServings] = useState(item ? item.servings : "");
   return (
     <div className={Class.container}>
       <form onKeyDown={formSubmitHandlerKeypress} className={Class.form}>
         <div className={Class.formGroup1}>
           <label className={Class.label}>Add Name</label>
-          <input type="text" onChange={inputNameHandler} className={Class.input} />
+          <input type="text" onChange={inputNameHandler} className={Class.input} value={inputName} />
         </div>
         <div className={Class.formGroup2}>
           <label className={Class.label}>Calories</label>
-          <input type="number" onChange={inputCaloriesHandler} className={Class.input} />
+          <input type="number" onChange={inputCaloriesHandler} className={Class.input} value={inputCalories} />
         </div>
         <div className={Class.formGroup3}>
           <label className={Class.label}>Servings</label>
-          <input type="number" onChange={inputServingsHandler} className={Class.input} />
+          <input type="number" onChange={inputServingsHandler} className={Class.input} value={inputServings} />
         </div>
         <BsFillArrowRightCircleFill className={Class.submitBtn} onClick={formSubmitHandlerClick} />
       </form>
--- client/src/components/diary/DiaryItem.jsx
+++ client/src/components/diary/DiaryItem.jsx
@@ -2,48 +2,69 @@ import Class from "./DiaryItem.module.css";
 import OptionsBtn from "./OptionsBtn";
 import {useState} from "react";
 import OptionsModal from "./OptionsModal";
-import {updateItem} from "../../apis/diaryApi";
+import DiaryForm from "./DiaryForm";
 
 function DiaryItem(props) {
+  // isActive controls options btn classes
   const [isActive, setIsActive] = useState(false);
-
   const setActiveHandler = () => {
     setIsActive(!isActive);
   };
 
+  // if isEditing is true display item form
+  const [isEditing, setIsEditing] = useState(false);
+
   const editClickHandler = () => {
-    // TODO: change update data to edit form data
-    const item = {
-      name: "WAPDY",
-      calories: 400,
-      _id: "6494eea4aa374f233612df90",
-      servings: 40,
-      date: "1/1/2023",
-      timeOfDay: "Breakfast",
-    };
-    updateItem(item);
+    setIsEditing(true);
+    setIsActive(false);
   };
 
   const deleteClickHandler = () => {};
 
+  const [name, setName] = useState(props.name);
+  const [calories, setCalories] = useState(props.calories);
+  const [servings, setServings] = useState(props.servings);
+
+  // when form is submitted it will call this function
+  const update = (name, serv, cals) => {
+    setName(name);
+    setCalories(cals);
+    setServings(serv);
+    setIsEditing(false);
+    console.log(calories, cals);
+    props.updateTotalCals(calories, cals);
+  };
+
   return (
-    <div className={Class.item}>
-      <div className={Class.itemGroup}>
-        <div className={Class.itemName}>{props.name}</div>
-        <sub className={Class.itemServings}>servings:{props.servings}</sub>
-      </div>
-      <div className={Class.actionGroup}>
-        <div className={Class.itemCalories}>{props.calories}</div>
-        <OptionsBtn onOptionsClick={setActiveHandler} isActive={isActive} />
-      </div>
-      {isActive && (
-        <OptionsModal
-          setIsActive={setActiveHandler}
-          onEditClick={editClickHandler}
-          onDeleteClick={deleteClickHandler}
+    <>
+      {!isEditing ? (
+        <div className={Class.item}>
+          <div className={Class.itemGroup}>
+            <div className={Class.itemName}>{name}</div>
+            <sub className={Class.itemServings}>servings:{servings}</sub>
+          </div>
+          <div className={Class.actionGroup}>
+            <div className={Class.itemCalories}>{calories}</div>
+            <OptionsBtn onOptionsClick={setActiveHandler} isActive={isActive} />
+          </div>
+          {isActive && (
+            <OptionsModal
+              setIsActive={setActiveHandler}
+              onEditClick={editClickHandler}
+              onDeleteClick={deleteClickHandler}
+            />
+          )}
+        </div>
+      ) : (
+        <DiaryForm
+          isEditing={isEditing}
+          item={{name: props.name, calories: props.calories, servings: props.servings}}
+          timeOfDay={props.timeOfDay}
+          _id={props._id}
+          editItem={update}
         />
       )}
-    </div>
+    </>
   );
 }
 
