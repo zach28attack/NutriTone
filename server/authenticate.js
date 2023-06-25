ecaedfabb61187e8c88be99caf770d6c52b431ee
Zachary Casares <rrumble250@gmail.com>
2023-06-25 11:53:54 -0400

    add try/catch to jwt verification

--- server/authenticate.js
+++ server/authenticate.js
@@ -4,14 +4,19 @@ const User = require("./models/user");
 exports.verifyToken = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
   if (token) {
-    const decoded = jwt.verify(token, "12341234");
-    const user = new User(); // initialize user object
-    user.id = decoded.id;
-    user.token = token;
-    const tokenValid = await user.validateToken(); // check if token is revoked
-    if (tokenValid) {
-      req.user = user; // if token is valid attach user obj to req obj
-      next();
+    try {
+      const decoded = jwt.verify(token, "12341234");
+      const user = new User(); // initialize user object
+      user.id = decoded.id;
+      user.token = token;
+      const tokenValid = await user.validateToken(); // check if token is revoked
+      if (tokenValid) {
+        req.user = user; // if token is valid attach user obj to req obj
+        next();
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(401).json();
     }
   } else {
     res.status(401).json();
