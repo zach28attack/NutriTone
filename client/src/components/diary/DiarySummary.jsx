e5979b42b32cae18d6b3ae6446bd3420df063eda
Zachary Casares <rrumble250@gmail.com>
2023-06-26 13:03:25 -0400

    add totals to diary summary

--- client/src/components/diary/DiarySummary.jsx
+++ client/src/components/diary/DiarySummary.jsx
@@ -1,6 +1,14 @@
 import Class from "./DiarySummary.module.css";
+import {useState, useEffect} from "react";
 
-function DiarySummary() {
+function DiarySummary(props) {
+  const [calories, setCalories] = useState(0);
+  const [caloriesBurned, setCaloriesBurned] = useState(0);
+  const [budget, setBudget] = useState(1850);
+
+  useEffect(() => {
+    setCalories(props.calories);
+  }, [props.calories]);
   return (
     <div className={Class.container}>
       <div className={Class.arrowLeft}>
@@ -15,16 +23,16 @@ function DiarySummary() {
           <li className={Class.headerItem}>Under</li>
         </ul>
         <ul className={Class.listValues}>
-          <li className={Class.valueItem}>1805</li>
+          <li className={Class.valueItem}>{budget}</li> {/* Budget */}
           <li className={Class.valueItem}>+</li>
-          <li className={Class.valueItem}>205</li>
+          <li className={Class.valueItem}>{calories}</li> {/* total calories logged */}
           <li className={Class.valueItem}>+</li>
-          <li className={Class.valueItem}>0</li>
+          <li className={Class.valueItem}>{caloriesBurned}</li> {/* calories burned */}
           <li className={Class.valueItem}>+</li>
-          <li className={Class.valueItem}>205</li>
+          <li className={Class.valueItem}>{calories - caloriesBurned}</li> {/* total cals consumed - burned */}
           <li className={Class.valueItem}>+</li>
           <li className={Class.valueItem}>
-            <div className={Class.calsUnder}>1600</div>
+            <div className={Class.calsUnder}>{budget - (calories - caloriesBurned)}</div> {/* total sum */}
           </li>
           {/* <li className={Class.valueItem}><div className={Class.calsOver}>1600</div></li> */}
         </ul>
