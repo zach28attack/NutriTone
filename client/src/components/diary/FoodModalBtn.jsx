41b5b0acd2f5a693255dae347a26c07b0e9049f5
Zachary Casares <rrumble250@gmail.com>
2023-06-27 12:31:29 -0400

    create foods button

---
+++ client/src/components/diary/FoodModalBtn.jsx
@@ -0,0 +1,22 @@
+import Class from "./FoodModalBtn.module.css";
+import {useState} from "react";
+import {createPortal} from "react-dom";
+import FoodModal from "./FoodModal";
+
+function FoodModalBtn(props) {
+  const [modalIsActive, setModalIsActive] = useState(false);
+  const btnClickHandler = () => {
+    props.setIsActive(false);
+    setModalIsActive(true);
+  };
+  return (
+    <div className={Class.container}>
+      <div className={`${Class.button} ${props.isActive && Class.active}`} onClick={btnClickHandler}>
+        Foods
+      </div>
+      {modalIsActive && createPortal(<FoodModal setIsActive={setModalIsActive} />, document.querySelector("#modal"))}
+    </div>
+  );
+}
+
+export default FoodModalBtn;
