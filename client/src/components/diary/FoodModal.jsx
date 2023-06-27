86ed82dc65281c0e1721cd533d6f1f2349ec2633
Zachary Casares <rrumble250@gmail.com>
2023-06-27 13:48:55 -0400

    style modal tabs

--- client/src/components/diary/FoodModal.jsx
+++ client/src/components/diary/FoodModal.jsx
@@ -1,14 +1,26 @@
 import Class from "./FoodModal.module.css";
+import ModalRecentContent from "./ModalRecentContent";
+import ModalSearchContent from "./ModalSearchContent";
+import {useState} from "react";
 
 function FoodModal(props) {
-  const clickHandler = () => {
-    props.setIsActive(false);
+  const clickHandler = (e) => {
+    if (e.target.className === Class.container) {
+      props.setIsActive(false);
+    }
   };
   const [searchTabActive, setSearchTabActive] = useState(true);
   return (
     <div className={Class.backgroundModal} onClick={clickHandler}>
       <div className={Class.container}>
-        <div className={Class.card}></div>
+        <div className={Class.card}>
+          <div className={Class.tabs}>
+            <div className={`${Class.tab} ${!searchTabActive && Class.activeTab}`}>Recent</div>
+            <div className={`${Class.tab} ${searchTabActive && Class.activeTab}`}>Search</div>
+          </div>
+
+          {searchTabActive ? <ModalSearchContent /> : <ModalRecentContent />}
+        </div>
       </div>
     </div>
   );
