fda5db65fe5e5e4bff388356a32cb5759ec65873
Zachary Casares <rrumble250@gmail.com>
2023-06-27 15:36:10 -0400

    search for item in usda db and render results

--- client/src/components/diary/ModalSearchContent.jsx
+++ client/src/components/diary/ModalSearchContent.jsx
@@ -1,14 +1,32 @@
 import Class from "./ModalSearchContent.module.css";
+import {useState} from "react";
+import {apiTest} from "../../apis/usdaApi";
+import ModalSearchItem from "./ModalSearchItem";
 
 function ModalSearchContent() {
+  const [searchInput, setSearchInput] = useState();
+  const inputHandler = (input) => {
+    setSearchInput(input.target.value);
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const items = await apiTest(searchInput);
+    setSearchResults(items);
+  };
+  const [searchResults, setSearchResults] = useState([]);
   return (
     <div className={Class.page}>
       <div className={Class.container}>
         <form className={Class.searchbar}>
-          <input type="text" className={Class.input} />
-          <input type="submit" className={Class.submit} />
+          <input type="text" className={Class.input} onChange={inputHandler} />
+          <input type="submit" className={Class.submit} onClick={submitHandler} />
         </form>
-        <div className={Class.content}></div>
+        <div className={Class.content}>
+          {console.log("results:", searchResults)}
+          {searchResults.map((item) => {
+            return <ModalSearchItem item={item} key={item.fdcId} />;
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/diary/ModalSearchItem.jsx
@@ -0,0 +1,7 @@
+import Class from "./ModalSearchItem.module.css";
+
+function ModalSearchItem(props) {
+  return <div>item:{props.item.description}</div>;
+}
+
+export default ModalSearchItem;
