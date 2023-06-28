ae2b04f0ce63e70d1210a0dc1a419d7eef39628d
Zachary Casares <rrumble250@gmail.com>
2023-06-28 11:33:08 -0400

    style recent search page

--- client/src/components/diary/ModalRecentContent.jsx
+++ client/src/components/diary/ModalRecentContent.jsx
@@ -1,7 +1,27 @@
 import Class from "./ModalRecentContent.module.css";
+import ModalSearchItem from "./ModalSearchItem";
 
-function ModalRecentContent() {
-  recent;
+function ModalRecentContent(props) {
+  console.log(props.items);
+  return (
+    <div className={Class.page}>
+      <div className={Class.container}>
+        <div className={Class.content}>
+          {props.items.map((item) => {
+            return (
+              <ModalSearchItem
+                item={item}
+                key={item.fdcId}
+                addItem={props.addItem}
+                timeOfDay={props.timeOfDay}
+                setIsActive={props.setIsActive}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ModalRecentContent;
