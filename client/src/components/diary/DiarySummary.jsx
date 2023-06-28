ac5cef62f1578ac8cfbd216a3f66b9d6b1a63607
Zachary Casares <rrumble250@gmail.com>
2023-06-28 15:08:03 -0400

    if cals logged exceed budget change outline

--- client/src/components/diary/DiarySummary.jsx
+++ client/src/components/diary/DiarySummary.jsx
@@ -5,15 +5,17 @@ function DiarySummary(props) {
   const [calories, setCalories] = useState(0);
   const [caloriesBurned, setCaloriesBurned] = useState(0);
   const [budget, setBudget] = useState(1850);
-
+  const [under, setUnder] = useState(0);
   useEffect(() => {
     setCalories(props.calories);
+    setUnder(budget - (props.calories - caloriesBurned));
   }, [props.calories]);
   return (
     <div className={Class.container}>
       <div className={Class.arrowLeft}>
         <div></div>
       </div>
+      <div></div>
       <div className={Class.summary}>
         <ul className={Class.summaryHeader}>
           <li className={Class.headerItem}>Budget</li>
@@ -32,9 +34,8 @@ function DiarySummary(props) {
           <li className={Class.valueItem}>{calories - caloriesBurned}</li> {/* total cals consumed - burned */}
           <li className={Class.valueItem}>+</li>
           <li className={Class.valueItem}>
-            <div className={Class.calsUnder}>{budget - (calories - caloriesBurned)}</div> {/* total sum */}
+            <div className={`${under >= 0 ? Class.calsUnder : Class.calsOver}`}>{under}</div> {/* total sum */}
           </li>
-          {/* <li className={Class.valueItem}><div className={Class.calsOver}>1600</div></li> */}
         </ul>
       </div>
       <div className={Class.arrowRight}>
