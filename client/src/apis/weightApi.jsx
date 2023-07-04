88a8d237e188bf3b5f9deaf6981a3d534c3eaa55
Zachary Casares <rrumble250@gmail.com>
2023-07-04 18:47:01 -0400

    create a saveNewWeightLog api function

--- client/src/apis/weightApi.jsx
+++ client/src/apis/weightApi.jsx
@@ -13,3 +13,19 @@ export async function getWeightLogs() {
     return data.logs;
   }
 }
+
+export async function saveNewWeightLog(log) {
+  const res = await fetch("http://localhost:3000/logs", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${Cookies.get("token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      log: log,
+    }),
+  });
+  if (res.ok) {
+    return true;
+  }
+}
