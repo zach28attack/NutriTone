74049940c563d7087cdc88bba59dad38e234fbea
Zachary Casares <rrumble250@gmail.com>
2023-07-04 17:22:12 -0400

    populate the chart with user weight logs

--- client/src/components/stats/ProgressChart.jsx
+++ client/src/components/stats/ProgressChart.jsx
@@ -10,9 +10,77 @@ import {
   Legend,
 } from "chart.js";
 import {Line} from "react-chartjs-2";
+import {useState, useEffect} from "react";
+import {getWeightLogs} from "../../apis/weightApi";
 
 function ProgressChart() {
   ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+  const [dateMonth, setDateMonth] = useState([
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ]);
+
+  // const [dateYear, setDateYear] = useState([
+  //    start from now to a data point from 1 year ago or the
+  //    oldest weightLog date to now, whatever is smaller
+  // ]);
+
+  const [logs, setLogs] = useState([]);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(300);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  useEffect(() => {
+    getLogsAndSetData();
+  }, []);
+
+  const getLogsAndSetData = async () => {
+    const logs = await getWeightLogs();
+    setLogs(logs);
+    setMin(
+      logs.reduce((prev, current) => {
+        return prev.weight < current.weight ? prev : current;
+      })
+    );
+
+    setMax(
+      logs.reduce((prev, current) => {
+        return prev.weight > current.weight ? prev : current;
+      })
+    );
+  };
+
+  const data = {
+    labels: dateMonth,
+    datasets: [
+      {
+        label: "Weight",
+        data: logs.map((log) => {
+          // change x: date to the selected sortBy timeframe:
+          // if(sortByMonth) else if (sortByYear) and so on...
+          return {x: new Date(log.date).toLocaleString("default", {month: "short"}), y: log.weight};
+        }),
+        borderColor: "#bf7d60",
+        backgroundColor: "#bf7d60",
+      },
+    ],
+  };
+
+  // options for the line chart
   const options = {
     responsive: true,
     plugins: {
@@ -27,24 +95,15 @@ function ProgressChart() {
     scales: {
       y: {
         ticks: {
+          // this function returns the y-axis value with ' lbs' attach, expl: '10 lbs'
           callback: function (value) {
             return value + " lbs";
           },
         },
+        suggestedMin: parseInt(min.weight) - 20,
+        suggestedMax: parseInt(max.weight) + 20,
       },
     },
-  };
-
-  const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul"],
-    datasets: [
-      {
-        label: "Weight",
-        data: [19, 200, 25, 150, 80, 120, 100],
-        borderColor: "#bf7d60",
-        backgroundColor: "#bf7d60",
-      },
-    ],
   };
   return (
     <div className={Class.chartContainer}>
@@ -52,6 +111,19 @@ function ProgressChart() {
         <div className={Class.chart}>
           <Line options={options} datasetIdKey="id" data={data} />
         </div>
+        <form className={Class.logForm} onSubmit={submitHandler}>
+          <div>
+            <label>Weight</label>
+            <input type="number" className={Class.weightInput} placeholder="lbs" />
+          </div>
+
+          <div>
+            <label>Date</label>
+            <input type="date" className={Class.dateInput} />
+          </div>
+
+          <input type="submit" className={Class.submitBtn} />
+        </form>
       </div>
     </div>
   );
