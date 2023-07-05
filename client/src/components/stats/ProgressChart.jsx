0c03b41219f9f4b5a5a1779505bde9320d54fd29
Zachary Casares <rrumble250@gmail.com>
2023-07-05 16:27:42 -0400

    sort data by timeframe selection

--- client/src/components/stats/ProgressChart.jsx
+++ client/src/components/stats/ProgressChart.jsx
@@ -15,70 +15,49 @@ import {getWeightLogs} from "../../apis/weightApi";
 import ChartForm from "./ChartForm";
 
 function ProgressChart() {
-  const [dateMonth, setDateMonth] = useState([
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ]);
-
-  // const [dateYear, setDateYear] = useState([]);
-  //    start from now to a data point from 1 year ago or the
-  //    oldest weightLog date to now, whatever is smaller
-
+  const [sortByFrame, setSortByFrame] = useState("30 days");
   const [logs, setLogs] = useState([]);
   const [min, setMin] = useState(0);
   const [max, setMax] = useState(300);
+  const [sortedLogsByDay, setSortedLogsByDay] = useState([]);
+  const [sortedLogsByMonth, setSortedLogsByMonth] = useState([]);
+  const [sortedLogsAll, setSortedLogsAll] = useState([]);
 
-  useEffect(() => {
-    getLogsAndSetData();
-  }, []);
-
-  const getLogsAndSetData = async () => {
-    const logs = await getWeightLogs();
-    const sortedLogs = logs.sort((a, b) => new Date(a.date) - new Date(b.date));
-    setLogs(sortedLogs);
-    setMin(
-      logs.reduce((prev, current) => {
-        return prev.weight < current.weight ? prev : current;
-      })
-    );
-
-    setMax(
-      logs.reduce((prev, current) => {
-        return prev.weight > current.weight ? prev : current;
+  const sortLogDatesByDay = (logs) => {
+    const thirtyDayFilter = new Date();
+    thirtyDayFilter.setDate(thirtyDayFilter.getDate() - 30);
+    const sortedLogs = logs.filter((log) => new Date(log.date) >= thirtyDayFilter);
+    setSortedLogsByDay(
+      sortedLogs.map((log) => {
+        return {x: log.date, y: log.weight};
       })
     );
   };
-
-  const addWeightLogHandler = (log) => {
-    setLogs((prevLogs) => {
-      const newLogs = [...prevLogs, log];
-      return newLogs.sort((a, b) => new Date(a.date) - new Date(b.date));
-    });
+  const sortLogDatesByMonth = (logs) => {
+    setSortedLogsByMonth(
+      logs.map((log) => {
+        return {x: log.date, y: log.weight};
+      })
+    );
   };
-
+  const sortAllLogs = (logs) => {
+    const latestLog = logs[logs.length - 1];
+    setSortedLogsAll(
+      logs.map((log) => {
+        return {x: log.date, y: log.weight};
+      })
+    );
+  };
   //
   // Line chart Config
   ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
   const data = {
-    labels: dateMonth,
+    labels: [],
     datasets: [
       {
         label: "Weight",
-        data: logs.map((log) => {
-          // change x: date to the selected sortBy timeframe:
-          // if(sortByMonth) else if (sortByYear) and so on...
-          return {x: new Date(log.date).toLocaleString("default", {month: "short"}), y: log.weight};
-        }),
+        data:
+          sortByFrame === "30 days" ? sortedLogsByDay : sortByFrame === "12 months" ? sortedLogsByMonth : sortedLogsAll,
         borderColor: "#bf7d60",
         backgroundColor: "#bf7d60",
       },
@@ -111,9 +90,58 @@ function ProgressChart() {
   };
   //
   //
+
+  const getLogsAndSetData = async () => {
+    const logs = await getWeightLogs();
+    const sortedLogs = logs.sort((a, b) => new Date(a.date) - new Date(b.date));
+    setLogs(sortedLogs);
+    setMin(
+      logs.reduce((prev, current) => {
+        return prev.weight < current.weight ? prev : current;
+      })
+    );
+
+    setMax(
+      logs.reduce((prev, current) => {
+        return prev.weight > current.weight ? prev : current;
+      })
+    );
+    sortLogDatesByDay(logs);
+    sortLogDatesByMonth(logs);
+    sortAllLogs(logs);
+  };
+
+  const addWeightLogHandler = (log) => {
+    setLogs((prevLogs) => {
+      const newLogs = [...prevLogs, log];
+      const sortedLogs = newLogs.sort((a, b) => new Date(a.date) - new Date(b.date));
+      sortLogDatesByDay(sortedLogs);
+      sortLogDatesByMonth(sortedLogs);
+      sortAllLogs(sortedLogs);
+      return sortedLogs;
+    });
+  };
+
+  const dateInputHandler = (e) => {
+    e.preventDefault();
+    setSortByFrame(e.target.value);
+  };
+
+  useEffect(() => {
+    getLogsAndSetData();
+  }, []);
+
   return (
     <div className={Class.chartContainer}>
       <div className={Class.chartCard}>
+        <form className={Class.sortBy}>
+          <label>Sort by:</label>
+          <select onChange={dateInputHandler}>
+            <option> 30 days </option>
+            <option> 12 months </option>
+            <option> All </option>
+          </select>
+        </form>
         <div className={Class.chart}>
           <Line options={options} datasetIdKey="id" data={data} />
         </div>
