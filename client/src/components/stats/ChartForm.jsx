efe8b301b2de9e70c32fa34595939c2ef9cb1865
Zachary Casares <rrumble250@gmail.com>
2023-07-05 16:27:16 -0400

    reset input values on form submit

--- client/src/components/stats/ChartForm.jsx
+++ client/src/components/stats/ChartForm.jsx
@@ -4,31 +4,34 @@ import {useState} from "react";
 
 function ChartForm(props) {
   const [date, setDate] = useState(new Date().toLocaleDateString());
+  const [datePlaceHolder, setDatePlaceHolder] = useState(new Date().toLocaleDateString());
   const [weight, setWeight] = useState();
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(date);
-    // saveNewWeightLog({date: date, weight: parseInt(weight)});
-    // props.addWeightLog({date: date, weight: parseInt(weight)});
+    saveNewWeightLog({date: date, weight: parseInt(weight)});
+    props.addWeightLog({date: date, weight: parseInt(weight)});
+    setWeight("");
+    setDatePlaceHolder("");
   };
   const weightHandler = (input) => {
     setWeight(input.target.value);
   };
   const dateHandler = (input) => {
+    setDatePlaceHolder(input.target.value);
     const date = new Date(input.target.value);
-    setDate(new Date(date.setDate(date.getDate() + 1)).toLocaleString());
+    setDate(new Date(date.setDate(date.getDate() + 1)).toLocaleDateString());
   };
 
   return (
     <form className={Class.logForm} onSubmit={submitHandler}>
       <div>
         <label>Weight</label>
-        <input type="number" className={Class.weightInput} placeholder="lbs" onChange={weightHandler} />
+        <input type="number" className={Class.weightInput} placeholder="lbs" onChange={weightHandler} value={weight} />
       </div>
 
       <div>
         <label>Date</label>
-        <input type="date" className={Class.dateInput} onChange={dateHandler} />
+        <input type="date" className={Class.dateInput} onChange={dateHandler} value={datePlaceHolder} />
       </div>
 
       <input type="submit" className={Class.submitBtn} />
