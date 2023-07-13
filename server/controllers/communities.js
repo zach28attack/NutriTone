dd4e801aee853879cb20d1e6587f2c18a93315b8
Zachary Casares <rrumble250@gmail.com>
2023-07-13 19:33:38 -0400

    get all joined communities

--- server/controllers/communities.js
+++ server/controllers/communities.js
@@ -1,1 +1,21 @@
-exports.getCommunity = (req, res, next) => {};
+const Community = require("../models/community");
+
+exports.getJoinedCommunities = async (req, res, next) => {
+  //TODO: limit joinedCommunities array to the most recent 10-20 posts and get more as user scrolls down page
+
+  const community = new Community();
+  const user = req.user;
+  community.userId = user.id;
+  try {
+    const joinedCommunities = await community.getJoinedCommunities();
+    if (joinedCommunities) {
+      res.status(200).json({
+        communities: joinedCommunities,
+      });
+    } else {
+      res.status(400).json();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
--- server/models/community.js
+++ server/models/community.js
@@ -1,3 +1,5 @@
+const {connectDB, closeConnection} = require("../database");
+const mongoDB = require("mongodb");
 class Community {
   constructor(id, name, userId, post, posts) {
     this.id = id;
@@ -6,4 +8,23 @@ class Community {
     this.post = post;
     this.posts = posts;
   }
+  async getJoinedCommunities() {
+    const db = await connectDB();
+    const userResult = await db.collection("users").findOne({_id: new mongoDB.ObjectId(this.userId)});
+    if (userResult) {
+      const communityIds = userResult.joinedCommunities.map(
+        (communitiy) => new mongoDB.ObjectId(communitiy.communityId)
+      );
+      const communitiesResult = await db
+        .collection("communities")
+        .find({_id: {$in: communityIds}})
+        .toArray();
+      closeConnection();
+      return communitiesResult;
+    }
+    closeConnection();
+    return false;
+  }
 }
+
+module.exports = Community;
