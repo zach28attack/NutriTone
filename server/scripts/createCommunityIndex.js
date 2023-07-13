735bca56edaa35fb66dcb852a137a99ddee1bac6
Zachary Casares <rrumble250@gmail.com>
2023-07-13 19:34:02 -0400

    add users.id and posts index

--- server/scripts/createCommunityIndex.js
+++ server/scripts/createCommunityIndex.js
@@ -2,7 +2,8 @@ const {connectDB, closeConnection} = require("../database");
 
 async function createIndex() {
   const db = await connectDB();
-  await db.collection("communities").createIndex({posts: 1, users: 1});
+  await db.collection("communities").createIndex({"users.userId": 1});
+  await db.collection("communities").createIndex({posts: 1});
   console.log("Community index created");
   process.exit(0);
 }
