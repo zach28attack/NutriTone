4158fe63a2cb563a7a8582abd18f7cc7d608f81d
Zachary Casares <rrumble250@gmail.com>
2023-07-13 12:08:53 -0400

    render posts in group page

--- client/src/pages/CommunityGroupPage.jsx
+++ client/src/pages/CommunityGroupPage.jsx
@@ -1,7 +1,19 @@
 import Class from "./CommunityGroupPage.module.css";
+import Post from "../components/community/Post";
 
 function CommunityGroupPage() {
-  return;
+  const user = {
+    name: "testing",
+    username: "@user990099",
+    body: "TESTING testing heeerrrrrrr",
+    img: "../../public/default-profile-picture1.jpg",
+  };
+  return (
+    <div className={Class.container}>
+      <h1>[Group Name]</h1>
+      <Post user={user} />
+    </div>
+  );
 }
 
 export default CommunityGroupPage;
