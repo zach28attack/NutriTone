21481e4d549a76117a61d3854b39bc5957bc929d
Zachary Casares <rrumble250@gmail.com>
2023-07-13 19:30:50 -0400

    add an api function to get all joined communities

---
+++ client/src/apis/communityApi.jsx
@@ -0,0 +1,16 @@
+import Cookies from "js-cookie";
+
+export async function getJoinedCommunities() {
+  const res = await fetch("http://localhost:3000/communities", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${Cookies.get("token")}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (res.ok) {
+    const data = await res.json();
+    console.log(data.communities);
+    return data.communities;
+  }
+}
