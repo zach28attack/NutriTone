f2a2df7ed424f58178970b5d834c3dbf20da8cbf
Zachary Casares <rrumble250@gmail.com>
2023-07-13 20:06:09 -0400

    add dynamic data to group page

--- client/src/pages/CommunityGroupPage.jsx
+++ client/src/pages/CommunityGroupPage.jsx
@@ -2,16 +2,21 @@ import Class from "./CommunityGroupPage.module.css";
 import Post from "../components/community/Post";
 
 function CommunityGroupPage() {
-  const user = {
+  // when communityItems are clicked in the menu call getCommunityPosts, get 10-20 posts
+  // when user scrolls to bottom of page get more posts from db
+
+  const DD = {
     name: "testing",
     username: "@user990099",
     body: "TESTING testing heeerrrrrrr",
     img: "../../public/default-profile-picture1.jpg",
+    date: "1/1/2023",
   };
   return (
     <div className={Class.container}>
+      <h6>*group page*</h6>
       <h1>[Group Name]</h1>
-      <Post user={user} />
+      <Post post={DD} />
     </div>
   );
 }
