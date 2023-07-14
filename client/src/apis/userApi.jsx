94d8df33e70d11032870f9844aca006a0d938b8a
Zachary Casares <rrumble250@gmail.com>
2023-07-14 14:43:42 -0400

    set name and username cookie

--- client/src/apis/userApi.jsx
+++ client/src/apis/userApi.jsx
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-export async function signup(email, username, password, passwordConfirmation) {
+export async function signup(email, name, username, password, passwordConfirmation) {
   try {
     const response = await fetch("http://localhost:3000/user/signup", {
       method: "POST",
@@ -9,6 +9,7 @@ export async function signup(email, username, password, passwordConfirmation) {
       },
       body: JSON.stringify({
         email: email,
+        name: name,
         username: username,
         password: password,
         passwordConfirmation: passwordConfirmation,
@@ -17,12 +18,16 @@ export async function signup(email, username, password, passwordConfirmation) {
 
     if (response.ok) {
       const data = await response.json();
+      const name = data.name;
+      const username = data.username;
       const today = new Date();
       const startOfYear = new Date(today.getFullYear(), 0, 1);
       const dayDate = Math.ceil((today - startOfYear) / (1000 * 60 * 60 * 24));
       Cookies.set("dayDate", dayDate, {expires: 1});
       Cookies.set("userId", data.id, {expires: 1});
       Cookies.set("token", data.token, {expires: 1});
+      Cookies.set("name", name, {expires: 1});
+      Cookies.set("username", username, {expires: 1});
 
       return true;
     } else {
@@ -46,6 +51,8 @@ export async function logout() {
       Cookies.remove("userId");
       Cookies.remove("token");
       Cookies.remove("dayDate");
+      Cookies.remove("name", name, {expires: 1});
+      Cookies.remove("username", username, {expires: 1});
       return true;
     } else {
       console.error("Issue done happened");
@@ -66,12 +73,16 @@ export async function login(username, password) {
     });
     if (response.ok) {
       const data = await response.json();
+      const name = data.name;
+      const username = data.username;
       const today = new Date();
       const startOfYear = new Date(today.getFullYear(), 0, 1);
       const dayDate = Math.ceil((today - startOfYear) / (1000 * 60 * 60 * 24));
       Cookies.set("dayDate", dayDate, {expires: 1});
       Cookies.set("token", data.token, {expires: 1});
       Cookies.set("userId", data.id, {expires: 1});
+      Cookies.set("name", name, {expires: 1});
+      Cookies.set("username", username, {expires: 1});
       return true;
     }
   } catch (error) {
