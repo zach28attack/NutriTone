37af72e5e781b8ddcc58f5206b0aeeb36f6fb7cd
Zachary Casares <rrumble250@gmail.com>
2023-07-17 20:48:22 -0400

    render menuForm on add form click

--- client/src/components/community/Menu.jsx
+++ client/src/components/community/Menu.jsx
@@ -4,11 +4,17 @@ import {useEffect, useState} from "react";
 import CommunityItem from "./CommunityItem";
 import Cookies from "js-cookie";
 import {PiListPlusLight} from "react-icons/pi";
+import MenuForm from "./MenuForm";
 
 function Menu({setGroupPageIsActive, joinedCommunities, isLoading, setActiveCommunityId}) {
+  const [formIsActive, setFormIsActive] = useState(false);
+
   const communityClickHandler = (id) => {
     setGroupPageIsActive(true);
     setActiveCommunityId(id);
+  };
+  const addCommunityClickHandler = () => {
+    setFormIsActive(!formIsActive);
   };
 
   return (
@@ -22,9 +28,10 @@ function Menu({setGroupPageIsActive, joinedCommunities, isLoading, setActiveComm
       </header>
       <section>
         <h3>
-          Joined Communities <PiListPlusLight className={Class.addCommunityBtn} />
+          Joined Communities <PiListPlusLight className={Class.addCommunityBtn} onClick={addCommunityClickHandler} />
         </h3>
         <div className={Class.communityList}>
+          {!formIsActive && <MenuForm />}
           {!isLoading &&
             joinedCommunities.map((community) => {
               return (
