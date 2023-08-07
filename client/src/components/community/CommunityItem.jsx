848e194f85e86d1eb513619c0d51d3a4a4ad00c6
Zachary Casares <rrumble250@gmail.com>
2023-08-07 14:46:42 -0400

    add join/leave buttons to community items

--- client/src/components/community/CommunityItem.jsx
+++ client/src/components/community/CommunityItem.jsx
@@ -1,14 +1,26 @@
+import {useState} from "react";
 import Class from "./CommunityItem.module.css";
 
-function CommunityItem({name, communityClickHandler, id}) {
+function CommunityItem({name, communityClickHandler, id, joinedList, joined, joinCommunity, leaveCommunity}) {
   const clickHandler = () => {
     communityClickHandler(id);
   };
+  const joinCommunityHandler = () => {
+    joinCommunity(id);
+  };
+  const leaveCommunityHandler = () => {
+    leaveCommunity(id);
+  };
   return (
-    <div className={Class.communityItem} onClick={clickHandler} id={id}>
-      <img src="/default-profile-picture1.jpg" />
-      <span>{name}</span>
-      <sub>joined</sub>
+    <div className={Class.communityItem} id={id}>
+      <span onClick={clickHandler}>{name}</span>
+      {joinedList ? (
+        <button onClick={leaveCommunityHandler}>leave</button>
+      ) : joined ? (
+        <sub>joined</sub>
+      ) : (
+        <button onClick={joinCommunityHandler}>Join</button>
+      )}
     </div>
   );
 }
