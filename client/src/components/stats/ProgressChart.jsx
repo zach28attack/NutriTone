87dd98b39705e783bd4f522e2a66d42960135815
Zachary Casares <rrumble250@gmail.com>
2023-08-08 14:50:31 -0400

    refactor/cleanup code

--- client/src/components/stats/ProgressChart.jsx
+++ client/src/components/stats/ProgressChart.jsx
@@ -27,39 +27,25 @@ function ProgressChart() {
     const thirtyDayFilter = new Date();
     thirtyDayFilter.setDate(thirtyDayFilter.getDate() - 30);
     const sortedLogs = logs.filter((log) => new Date(log.date) >= thirtyDayFilter);
-
-    setSortedLogsByDay(
-      sortedLogs.map((log) => {
-        return {x: log.date, y: log.weight};
-      })
-    );
+    setSortedLogsByDay(sortedLogs.map((log) => ({x: log.date, y: log.weight})));
   };
   const filterLogsTwelveMonths = (logs) => {
     const twelveMonthFilter = new Date();
     twelveMonthFilter.setDate(twelveMonthFilter.getDate() - 30 * 12);
     const sortedLogs = logs.filter((log) => new Date(log.date) >= twelveMonthFilter);
 
-    setSortedLogsByMonth(
-      sortedLogs.map((log) => {
-        return {x: log.date, y: log.weight};
-      })
-    );
+    setSortedLogsByMonth(sortedLogs.map((log) => ({x: log.date, y: log.weight})));
   };
   const sortAllLogs = (logs) => {
-    setSortedLogsAll(
-      logs.map((log) => {
-        return {x: log.date, y: log.weight};
-      })
-    );
+    setSortedLogsAll(logs.map((log) => ({x: log.date, y: log.weight})));
   };
+
   //
   // Line chart Config
-
+  //
   ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
   const handleDataPointClick = (e, element) => {
-    if (element[0]) {
-      removeLog(element[0].index);
-    }
+    if (element[0]) removeLog(element[0].index);
   };
   const removeLog = (index) => {
     const logToDelete =
@@ -70,10 +56,7 @@ function ProgressChart() {
         : sortedLogsAll[index];
     deleteWeightLog({date: logToDelete.x, weight: logToDelete.y});
     setLogs((prevLogs) => {
-      const updateLogs = prevLogs.filter((log) => {
-        return !(log.date === logToDelete.x && log.weight === logToDelete.y);
-      });
-
+      const updateLogs = prevLogs.filter((log) => !(log.date === logToDelete.x && log.weight === logToDelete.y));
       filterLogsThirtyDays(updateLogs);
       filterLogsTwelveMonths(updateLogs);
       sortAllLogs(updateLogs);
@@ -125,17 +108,8 @@ function ProgressChart() {
     const logs = await getWeightLogs();
     const sortedLogs = logs.sort((a, b) => new Date(a.date) - new Date(b.date));
     setLogs(sortedLogs);
-    setMin(
-      logs.reduce((prev, current) => {
-        return prev.weight < current.weight ? prev : current;
-      })
-    );
-
-    setMax(
-      logs.reduce((prev, current) => {
-        return prev.weight > current.weight ? prev : current;
-      })
-    );
+    setMin(logs.reduce((prev, current) => (prev.weight < current.weight ? prev : current)));
+    setMax(logs.reduce((prev, current) => (prev.weight > current.weight ? prev : current)));
     filterLogsThirtyDays(logs);
     filterLogsTwelveMonths(logs);
     sortAllLogs(logs);
