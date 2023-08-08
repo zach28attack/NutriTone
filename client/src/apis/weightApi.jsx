ed5b5b5d9333f7d115ead4e0765ca5023164fec7
Zachary Casares <rrumble250@gmail.com>
2023-08-08 12:13:17 -0400

    add error handling to promises

--- client/src/apis/weightApi.jsx
+++ client/src/apis/weightApi.jsx
@@ -1,47 +1,63 @@
 import Cookies from "js-cookie";
 
 export async function getWeightLogs() {
-  const res = await fetch("http://localhost:3000/logs", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${Cookies.get("token")}`,
-      "Content-Type": "application/json",
-    },
-  });
-  if (res.ok) {
-    const data = await res.json();
-    return data.logs;
+  try {
+    const res = await fetch("http://localhost:3000/logs", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.ok) {
+      const data = await res.json();
+      return data.logs;
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
 export async function saveNewWeightLog(log) {
-  const res = await fetch("http://localhost:3000/logs", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${Cookies.get("token")}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      log: log,
-    }),
-  });
-  if (res.ok) {
-    return true;
+  try {
+    const res = await fetch("http://localhost:3000/logs", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        log: log,
+      }),
+    });
+    if (res.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
 export async function deleteWeightLog(log) {
-  const res = await fetch("http://localhost:3000/logs", {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${Cookies.get("token")}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      log: log,
-    }),
-  });
-  if (res.ok) {
-    return true;
+  try {
+    const res = await fetch("http://localhost:3000/logs", {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        log: log,
+      }),
+    });
+    if (res.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
