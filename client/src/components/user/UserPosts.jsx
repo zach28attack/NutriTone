ec53e5f211fd396f8eac41752fe827e78599e6e4
Zachary Casares <rrumble250@gmail.com>
2023-08-10 15:41:51 -0400

    render a list of the user's posts

--- client/src/components/user/UserPosts.jsx
+++ client/src/components/user/UserPosts.jsx
@@ -1,17 +1,29 @@
-import {useEffect, useState, useContext} from "react";
+import {useContext} from "react";
 import Class from "./UserPosts.module.css";
 import Post from "../community/Post";
 import {GlobalContext} from "../../context/GlobalContext";
-
+import Cookies from "js-cookie";
 function UserPosts() {
   const {posts} = useContext(GlobalContext);
-  console.log(posts);
+  const userId = Cookies.get("userId");
 
   return (
-    <div className={Class.Feed}>
-      {posts.map((post) => (
-        <Post post={post} groupName={post.communityName} key={post._id} id={post._id} communityId={post.communityId} />
-      ))}
+    <div className={Class.container}>
+      <div className={Class.feed}>
+        <h1>My posts</h1>
+        {posts.map((post) => {
+          if (post.userId === userId)
+            return (
+              <Post
+                post={post}
+                groupName={post.communityName}
+                key={post._id}
+                id={post._id}
+                communityId={post.communityId}
+              />
+            );
+        })}
+      </div>
     </div>
   );
 }
