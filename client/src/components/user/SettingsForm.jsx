e6f355cbc414469f398c9a5b1df3ba1f8bd37855
Zachary Casares <rrumble250@gmail.com>
2023-08-14 15:33:49 -0400

    create settingsForm

---
+++ client/src/components/user/SettingsForm.jsx
@@ -0,0 +1,14 @@
+import Class from "./SettingsForm.module.css";
+function SettingsForm() {
+  return (
+    <>
+      <p>change password</p>
+      <p>change profile pic</p>
+      <p>delete account</p>
+      <p>cancel subscription</p>
+      <p>manage payment</p>
+    </>
+  );
+}
+
+export default SettingsForm;
