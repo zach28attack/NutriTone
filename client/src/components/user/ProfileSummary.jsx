1b9f1ef3d4fc0030fb8b588069a3898c7491c8bc
Zachary Casares <rrumble250@gmail.com>
2023-08-14 15:21:24 -0400

    use weightlogs context api in user's summary

--- client/src/components/user/ProfileSummary.jsx
+++ client/src/components/user/ProfileSummary.jsx
@@ -1,18 +1,31 @@
 import Class from "./ProfileSummary.module.css";
+import {useContext, useEffect, useState} from "react";
+import {GlobalContext} from "../../context/GlobalContext";
 
 function ProfileSummary() {
+  const {logs, firstLog, lastLog} = useContext(GlobalContext);
+  // const [startWeight, setStartWeight] = useState(0);
+  // const [currentWeight, setCurrentWeight] = useState(0);
+
+  // useEffect(() => {
+  //   setStartWeight(firstLog);
+  //   setCurrentWeight(lastLog);
+  // }, [logs]);
+
   return (
     <summary className={Class.container}>
       <span className={Class.startWeightTitle}>Starting W.</span>
-      <span className={Class.startWeightData}>315lbs</span>
+      <span className={Class.startWeightData}>{firstLog}lbs</span>
       <div className={Class.horizontalDivider1}></div>
 
       <span className={Class.currentWeightTitle}>Current W.</span>
-      <span className={Class.currentWeightData}>300lbs</span>
+      <span className={Class.currentWeightData}>{lastLog}lbs</span>
       <div className={Class.horizontalDivider2}></div>
 
-      <span className={Class.goalWeightTitle}>Goal W.</span>
-      <span className={Class.goalWeightData}>185lbs</span>
+      <span className={Class.goalWeightTitle}>Progress</span>
+      <span className={Class.goalWeightData}>
+        {firstLog > lastLog ? `- ${firstLog - lastLog}` : `+ ${lastLog - firstLog}`} lbs
+      </span>
       <div className={Class.verticalDivider}></div>
     </summary>
   );
