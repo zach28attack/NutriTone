0fc3ed35638307b54859f0eb3e62dc8881aa9f39
Zachary Casares <rrumble250@gmail.com>
2023-08-16 21:16:00 -0400

    remove  Katch-McArdle formula from coach summary

--- client/src/pages/CoachPage.jsx
+++ client/src/pages/CoachPage.jsx
@@ -9,11 +9,11 @@ function CoachPage() {
         <h2>Your Personalized Calorie Intake Guide</h2>
         <h3>How Coach Works:</h3>
         <article>
-          Using formulas such as the Harris-Bernedict, Miflin-St Jeor, and Katch-McArdle formulas to calculate your
-          Basal Metabolic Rate (BMR). Then by taking into account your current weight, goal weight, preferred intensity
-          level, and gender, Coach formulates a tailor-made calorie intake goal that aligns perfectly with your health
-          fitness goals. Whether you're looking to lose weight steadily, embark on an intense fitness journey, or just
-          sustain your ideal physique, Coach has got you covered.
+          Using formulas such as the Harris-Bernedict or Miflin-St Jeor formulas to calculate your Basal Metabolic Rate
+          (BMR). Then by taking into account your current weight, goal weight, preferred intensity level, and gender,
+          Coach formulates a tailor-made calorie intake goal that aligns perfectly with your health fitness goals.
+          Whether you're looking to lose weight steadily, embark on an intense fitness journey, or just sustain your
+          ideal physique, Coach has got you covered.
         </article>
         <Calculator />
       </section>
