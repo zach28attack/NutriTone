383b2858816cd77b86107cf24732d34bcd27c477
Zachary Casares <rrumble250@gmail.com>
2023-08-17 13:30:08 -0400

    style calc resutls

--- client/src/components/coach/CalculatorResults.jsx
+++ client/src/components/coach/CalculatorResults.jsx
@@ -1,7 +1,22 @@
 import Class from "./CalculatorResults.module.css";
 
 function CalculatorResults() {
-  return <>resutlss</>;
+  return (
+    <div className={Class.container}>
+      <header>
+        <h4>Your custom budget</h4>
+        <h1>1800 cals</h1>
+      </header>
+      <section>
+        <button>Use Budget</button>
+        <button>Re-do</button>
+      </section>
+      <sub>
+        With the information you've input we've calculated a caloric budget custom tuned to your needs and goals. Please
+        speak to your health care provider if you have any health issues.
+      </sub>
+    </div>
+  );
 }
 
 export default CalculatorResults;
