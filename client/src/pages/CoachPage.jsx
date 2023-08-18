183311937815142b5719584ca7d4583c9a829937
Zachary Casares <rrumble250@gmail.com>
2023-08-18 19:35:34 -0400

    fix typo

--- client/src/pages/CoachPage.jsx
+++ client/src/pages/CoachPage.jsx
@@ -5,12 +5,11 @@ function CoachPage() {
   return (
     <div className={Class.page}>
       <section>
-        <h1>Coach</h1>
-        <h2>Your Personalized Calorie Intake Guide</h2>
+        <h1>Your Personalized Calorie Intake Guide</h1>
         <h3>How Coach Works:</h3>
         <article>
-          Using the trusted Harris-Bernedict formula to calculate your Basal Metabolic Rate (BMR) and taking into
-          account your current weight, goal weight, activity level, preferred intensity, and gender Coach formulates a
+          Using the trusted Harris-Benedict formula to calculate your Basal Metabolic Rate (BMR) and taking into account
+          your current weight, goal weight, activity level, preferred intensity, and gender Coach formulates a
           tailor-made calorie intake goal that aligns perfectly with your health fitness goals. Whether you're looking
           to lose weight steadily, embark on an intense fitness journey, or just sustain your ideal physique, Coach has
           got you covered.
