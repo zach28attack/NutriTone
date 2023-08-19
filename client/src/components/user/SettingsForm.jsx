dc8fd402afdbbbad503f6d0947479ae6a0d4674a
Zachary Casares <rrumble250@gmail.com>
2023-08-19 17:09:51 -0400

    remove dead code

--- client/src/components/user/SettingsForm.jsx
+++ client/src/components/user/SettingsForm.jsx
@@ -3,7 +3,6 @@ function SettingsForm() {
   return (
     <>
       <p>change password</p>
-      <p>change profile pic</p>
       <p>delete account</p>
       <p>cancel subscription</p>
       <p>manage payment</p>
