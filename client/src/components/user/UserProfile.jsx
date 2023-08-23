5082e6f4c7565c27807152ada881b6757f4bbce1
Zachary Casares <rrumble250@gmail.com>
2023-08-23 15:07:55 -0400

    remove edit btn on name/username

--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -1,5 +1,4 @@
 import Class from "./UserProfile.module.css";
-import {PiPencilFill} from "react-icons/pi";
 import {useState, useEffect} from "react";
 import {BiDotsVerticalRounded} from "react-icons/bi";
 import AccountModal from "../user/AccountModal";
@@ -9,30 +8,10 @@ import Cookies from "js-cookie";
 import {getProfilePic} from "../../apis/userApi";
 
 function UserProfile() {
-  const [nameIsActive, setNameIsActive] = useState();
-  const [usernameIsActive, setUsernameIsActive] = useState();
   const [accountModalIsActive, setAccountModalIsActive] = useState();
   const [name, setName] = useState(Cookies.get("name"));
   const [username, setUsername] = useState(Cookies.get("username"));
   const [image, setImage] = useState();
-
-  // when user hovers over name/username display edit button
-  const displayEditButtonHandler = (e) => {
-    if (e.target.closest("#name")) {
-      setNameIsActive(true);
-    } else if (e.target.closest("#username")) {
-      setUsernameIsActive(true);
-    }
-  };
-
-  // when user stops hovering over name/username hide edit button
-  const hideEditButtonHandler = (e) => {
-    if (e.target.closest("#name")) {
-      setNameIsActive(false);
-    } else if (e.target.closest("#username")) {
-      setUsernameIsActive(false);
-    }
-  };
 
   const accountClickHandler = () => {
     setAccountModalIsActive(!accountModalIsActive);
@@ -51,25 +30,9 @@ function UserProfile() {
         <article className={Class.profile}>
           <img src={image ? image : "blank.png"} alt="user profile pic" />
           <div className={Class.nameGroup}>
-            <div
-              className={Class.nameContainer}
-              onMouseOver={displayEditButtonHandler}
-              onMouseOut={hideEditButtonHandler}
-              id="name"
-            >
-              <h4>{name}</h4>
-              {nameIsActive && <PiPencilFill className={Class.editBtn} />}
-            </div>
+            <h4>{name}</h4>
             <div className={Class.divider}></div>
-            <div
-              className={Class.nameContainer}
-              onMouseOver={displayEditButtonHandler}
-              onMouseOut={hideEditButtonHandler}
-              id="username"
-            >
-              <span>@{username}</span>
-              {usernameIsActive && <PiPencilFill className={Class.editBtn} />}
-            </div>
+            <span>@{username}</span>
           </div>
           <BiDotsVerticalRounded className={Class.accountEdit} onClick={accountClickHandler} />
           {accountModalIsActive && (
