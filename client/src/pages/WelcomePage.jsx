cb38840d6edd0d3564e323ea068bd3d2cd54e9d8
Zachary Casares <rrumble250@gmail.com>
2023-08-28 18:10:08 -0400

    add photo to landing page

--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -1,13 +1,13 @@
 import Class from "./WelcomePage.module.css";
-import UserSignup from "../components/user/Signup";
-import UserLogin from "../components/user/Login";
+import SignupCard from "../components/user/SignupCard";
 
 function WelcomePage() {
   return (
-    <div className={Class.jumbotron}>
-      <UserSignup />
-      <div className={Class.divider}></div>
-      <UserLogin />
+    <div className={Class.container}>
+      <img src="/pexels-jonathan-borba-3622478.jpg" alt="oatmel on wooden table" />
+      <div className={Class.background}>
+        <SignupCard />
+      </div>
     </div>
   );
 }
